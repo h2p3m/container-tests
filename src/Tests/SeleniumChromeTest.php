503986a30341ea8b00058fc3e8b79029b7d8ef2e
Hans-Peter Martini <hans-peter.martini@scrutinizer-ci.com>
2017-06-08 12:15:21 +0200

    Update SeleniumChromeTest.php

--- src/Tests/SeleniumChromeTest.php
+++ src/Tests/SeleniumChromeTest.php
@@ -1,22 +1,14 @@
 <?php
-
 class SeleniumChromeTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-
     protected function setUp()
     {
         $this->setBrowser('chrome');
         $this->setBrowserUrl('http://google.com/');
     }
-
     public function testTitle()
     {
-        for ($i = 0; $i < 10; $i++)
-        {
-            $this->url('http://www.google.com/');
-            $this->assertContains('Google', $this->title());
-            $this->url('http://www.heise.de/');
-        }
+        $this->url('http://www.google.com/');
+        $this->assertContains('Google', $this->title());
     }
-
 }
